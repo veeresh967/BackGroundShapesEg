b477c4634fcf50f2a99678e5d21731428404906e
veeresh <veereshmc967@gmail.com>
2018-11-26 02:42:29 +0530

    Initial commit

---
+++ app/src/main/java/com/veeresh/b37_backgroundshapeseg/MainActivity.java
@@ -0,0 +1,13 @@
+package com.veeresh.b37_backgroundshapeseg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
